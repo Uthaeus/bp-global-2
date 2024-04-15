5ec458a0d7e26e5510b524d848754e79c775a102
Uthaeus <romanlavery@gmail.com>
2024-04-15 12:12:39 -0600

    added styling to auth components, added routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Error from "./components/error/error";
 import RootLayout from "./components/layouts/root-layout";
 import Home from "./pages/home";
+import Signup from "./components/auth/signup";
+import Login from "./components/auth/login";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +15,14 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />
+      },
+      {
+        path: "/signup",
+        element: <Signup />
+      },
+      {
+        path: "/login",
+        element: <Login />
       }
     ]
   }
