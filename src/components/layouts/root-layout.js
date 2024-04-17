937aba9cd53a94a3f5f0d2be0226a57a719d3677
Uthaeus <romanlavery@gmail.com>
2024-04-17 10:29:54 -0600

    added styling to footer and copyright, added footer to root layout

--- src/components/layouts/root-layout.js
+++ src/components/layouts/root-layout.js
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router";
 
 import MainNavigation from "../main-navigation";
+import MainFooter from "../footer/main-footer";
+import Copyright from "../footer/copyright";
 
 export default function RootLayout() {
     return (
         <div className="root-layout">
             <MainNavigation />
             <Outlet />
+            <MainFooter />
+            <Copyright />
         </div>
     );
 }
