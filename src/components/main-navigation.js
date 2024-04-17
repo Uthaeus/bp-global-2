af8b1de352e403fdaa32c7a8e126693a91d88886
Uthaeus <romanlavery@gmail.com>
2024-04-17 11:15:25 -0600

    refactoring and restyling main navigation

--- src/components/main-navigation.js
+++ src/components/main-navigation.js
@@ -1,20 +1,29 @@
 import { NavLink, Link } from "react-router-dom";
 
-import logo from '../assets/images/bp_global_logo2.png';
+import logo from '../assets/images/bp_global_logo.png';
 
 export default function MainNavigation() {
     
     return (
         <div className="main-navigation">
             <div className="main-navigation-left">
-                <Link to="/" className="nav-logo"><img src={logo} alt="logo" className="nav-logo-image" /></Link>
+                <Link to='/' className="navigation-logo-link" end>
+                    <img src={logo} alt='nav-logo' width='80%' />
+                </Link>
             </div>
 
             <div className="main-navigation-right">
-                <NavLink to="/" className={({ isActive }) => isActive ? "nav-link link-active" : "nav-link"}>Home</NavLink>
-                <NavLink to="/about" className={({ isActive }) => isActive ? "nav-link link-active" : "nav-link"}>About</NavLink>
-                <NavLink to="/login" className={({ isActive }) => isActive ? "nav-link link-active" : "nav-link"}>Log in</NavLink>
-                <NavLink to="/signup" className={({ isActive }) => isActive ? "nav-link link-active" : "nav-link"}>Sign up</NavLink>
+                <p className="navigation-phone">800-555-5555</p>
+
+                <div className="navigation-links-wrapper">
+
+                    <NavLink to='/about' className={({isActive}) => isActive ? 'nav-link link-active' : 'nav-link'}>About Us</NavLink>
+                    <NavLink to='/contact' className={({isActive}) => isActive ? 'nav-link link-active' : 'nav-link'}>Contact Us</NavLink>
+
+                    {isLoggedIn && (
+                        <Link to='/account' className="nav-link account-link">My Account</Link>
+                    )}
+                </div>
             </div>
         </div>
     )
