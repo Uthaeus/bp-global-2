366f78c989cedc32f496cd0088461f4104e2e35e
Uthaeus <romanlavery@gmail.com>
2024-04-18 13:26:39 -0600

    building new order form

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -1,14 +1,56 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+
+import { getDocs, collection } from "firebase/firestore";
+
+import { db } from "../../firebase";
 
 import { UserContext } from "../../store/user-context";
 
 function Admin() {
+    const navigate = useNavigate();
+    const [users, setUsers] = useState([]);
+    const [newOrder, setNewOrder] = useState(false);
 
     const { user } = useContext(UserContext);
 
+    useEffect(() => {
+        const getUsers = async () => {
+            try {
+                const q = query(collection(db, "users"));
+                const querySnapshot = await getDocs(q);
+                const users = [];
+
+                querySnapshot.forEach((doc) => {
+                    users.push({ ...doc.data(), id: doc.id });
+                });
+
+                setUsers(users);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        getUsers();
+    }, []);
+
     return (
         <div className="admin">
-            <h1 className="admin-title">Admin for {user.email}</h1>
+            <div className="admin-header">
+                <h1 className="admin-title">Admin for {user.email}</h1>
+            </div>
+
+            <div className="admin-content">
+                {!newOrder && (
+                    <button className="btn btn-primary admin-button" onClick={() => setNewOrder(true)}>Create new order</button>
+                )}
+
+                {newOrder && (
+                    // TODO: Create new order form
+                    
+                    <button className="btn btn-danger admin-button" onClick={() => setNewOrder(false)}>Close</button>
+                )}
+            </div>
         </div>
     );
 }
