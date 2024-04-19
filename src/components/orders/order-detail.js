29020769c956c632ecbdc41c6cd52be29fe5bb19
Uthaeus <romanlavery@gmail.com>
2024-04-19 13:09:29 -0600

    added initial action buttons to order detail for admin user

--- src/components/orders/order-detail.js
+++ src/components/orders/order-detail.js
@@ -1,12 +1,15 @@
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../../firebase";
 
+import { UserContext } from "../../store/user-context";
+
 function OrderDetail() {
     const { id } = useParams();
     const [order, setOrder] = useState({});
+    const { user, isAdmin } = useContext(UserContext);
 
     useEffect(() => {
         const getOrder = async () => {
@@ -24,6 +27,13 @@ function OrderDetail() {
     return (
         <div className="order-detail">
             <h2 className="order-detail-title">Order #{order.order_number}</h2>
+
+            {isAdmin && (
+                <div className="order-detail-buttons">
+                    <button className="btn btn-info">Edit</button>
+                    <button className="btn btn-danger">Delete</button>
+                </div>
+            )}
         </div>
     );
 }
