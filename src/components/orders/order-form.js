685397ae9075f7f3a8528b75df8d5beed46b08c5
Uthaeus <romanlavery@gmail.com>
2024-04-20 14:54:44 -0600

    refactoring order form submit handler

--- src/components/orders/order-form.js
+++ src/components/orders/order-form.js
@@ -8,7 +8,7 @@ import { storage } from "../../firebase";
 import { db } from "../../firebase";
 
 function OrderForm({ order, customers }) {
-    const [images, setImages] = useState([]);
+    // const [images, setImages] = useState([]);
     const [customer, setCustomer] = useState("");
 
     const { register, handleSubmit, formState: { errors }, reset } = useForm();
@@ -32,12 +32,62 @@ function OrderForm({ order, customers }) {
 
         console.log('order form submit data',data);
 
+        if (order) {
+
+            updateDoc(doc(db, "orders", order.id), {
+                ...data
+            });
+        } else {
+            const userRef = doc(db, "users", customer.id);
+
+            updateDoc(userRef, {
+                orders: arrayUnion({
+                    ...data,
+                }),
+            });
+
+            setDoc(doc(db, "orders"), {
+                ...data,
+            });
+        }
+
+        reset();
         
     }
 
     return (
         <div className="order-form-container">
             
+            <form className="order-form" onSubmit={handleSubmit(onSubmit)}>
+                <div className="row">
+                    <div className="col-6">
+                        <div className="form-group">
+                            <label htmlFor="customer">Customer</label>
+                            <select id="customer" className="form-select" {...register("customer", { required: true })} onChange={(e) => setCustomer(e.target.value)}>
+                                <option value="">Select customer</option>
+                                {customers.map((customer) => (
+                                    <option key={customer.id} value={customer.id}>{customer.name}</option>
+                                ))}
+                            </select>
+                            {errors.customer && <span className="text-danger">This field is required</span>}
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <div className="form-group">
+                            <label htmlFor="order_number">Order number</label>
+                            <input 
+                                type="text"
+                                id="order_number"
+                                className="form-control"
+                                {...register("order_number", { required: true })}
+                            />
+                            {errors.order_number && <span className="text-danger">This field is required</span>}
+                        </div>
+                    </div>
+                </div>
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </div>
     )
 }
