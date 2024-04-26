d34f11e428b95662df7aa94e9e1848e4a7163629
Uthaeus <romanlavery@gmail.com>
2024-04-26 12:58:23 -0600

    added order images container to order detail, created deleteOrderHandler function, added edit link

--- src/components/orders/order-detail.js
+++ src/components/orders/order-detail.js
@@ -1,37 +1,44 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect, useContext } from "react";
 
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "../../firebase";
-
 import { UserContext } from "../../store/user-context";
+import { OrdersContext } from "../../store/orders-context";
 
 function OrderDetail() {
     const { id } = useParams();
+    const { orders, deleteOrder } = useContext(OrdersContext);
+    const navigate = useNavigate();
     const [order, setOrder] = useState({});
     const { user, isAdmin } = useContext(UserContext);
 
     useEffect(() => {
-        const getOrder = async () => {
-            const docRef = doc(db, "orders", id);
-            const docSnap = await getDoc(docRef);
+        setOrder(orders.find((order) => order.id === id));
+    }, [orders, id]);
 
-            if (docSnap.exists()) {
-                setOrder({ ...docSnap.data(), id: docSnap.id });
-            }
+    const deleteOrderHandler = async () => {
+        try {
+            deleteOrder(id);
+        } catch (error) {
+            console.log('delete order error: ', error);
+        } finally {
+            navigate("/admin");
         }
-
-        getOrder();
-    }, [id]);
+    }
 
     return (
         <div className="order-detail">
             <h2 className="order-detail-title">Order #{order.order_number}</h2>
 
+            <div className="order-images-container">
+                {order.images.map((image) => (
+                    <img key={image} className="order-image" src={image} alt="Order Image" />
+                ))}
+            </div>
+
             {isAdmin && (
                 <div className="order-detail-buttons">
-                    <button className="btn btn-info">Edit</button>
-                    <button className="btn btn-danger">Delete</button>
+                    <button className="btn btn-info" onClick={() => navigate(`/orders/${id}/edit`)}>Edit</button>
+                    <button className="btn btn-danger" onClick={deleteOrderHandler}>Delete</button>
                 </div>
             )}
         </div>
